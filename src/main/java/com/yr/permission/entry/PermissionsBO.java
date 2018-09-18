5e920383ecf07db39ad4ef4985b5d93f7e3cbf78
liubojianggit <981011153@qq.com>
2018-09-18 19:25:06 +0800

    shiro entry BO add

---
+++ src/main/java/com/yr/permission/entry/PermissionsBO.java
@@ -0,0 +1,13 @@
+package com.yr.permission.entry;
+
+public class PermissionsBO {
+    private Permissions permissions;
+
+    public Permissions getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Permissions permissions) {
+        this.permissions = permissions;
+    }
+}
