dc513c3d387d00638feb887af674cff2f67c87f5
liubojianggit <981011153@qq.com>
2018-09-18 20:38:11 +0800

    role&permission add

--- src/main/java/com/yr/role/service/RoleServiceImpl.java
+++ src/main/java/com/yr/role/service/RoleServiceImpl.java
@@ -1,4 +1,19 @@
 package com.yr.role.service;
 
-public class RoleServiceImpl {
+import com.yr.entitys.Roles;
+import com.yr.entitys.bo.RolesBO;
+import com.yr.entitys.page.Page;
+import com.yr.role.dao.RoleDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    @Autowired
+    private RoleDao dao;
+
+    @Override
+    public Roles getRoleByPage(Page<RolesBO> page) {
+        return dao.getRoleByPage(page);
+    }
 }
